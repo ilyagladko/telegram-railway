b0ae007f0d142c070ed4f18ff9f557b293953323
ilyagladko <39254405+ilyagladko@users.noreply.github.com>
2022-03-21 21:33:41 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { Telegraf } from "telegraf";
+//const axios = require('axios').default;
+import axios from "axios";
+
+
 
 /*
   TELEGRAM_BOT_TOKEN is an environment variable
@@ -9,10 +13,42 @@ import { Telegraf } from "telegraf";
 if (!process.env.TELEGRAM_BOT_TOKEN) throw new Error("Please add a bot token");
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
+const url = 'https://collectionapi.metmuseum.org/public/collection/v1/';
+let img, object;
+
 bot.start(ctx => ctx.reply("Welcome"));
-bot.hears("hello", ctx => {
-  ctx.reply("Hello to you too!");
-});
+//bot.hears("hello", ctx => {
+//  ctx.reply("Hello to you too!");
+//});
+
+bot.on("text", async(ctx) => {
+    ctx.reply("сек...")
+    await getRandomID(ctx, ctx.message.text)
+})
+
+
+function getRandomID(ctx, query) {
+  axios.get(url + 'search?hasImages=true&q=' + query)
+  .then(res => {
+
+    let random = Math.round(Math.random() * res.data.total);
+    object = res.data.objectIDs[random];
+
+    axios.get(url + 'objects/' + object)
+    .then(res => {
+      img = res.data.primaryImage
+  
+//  ctx.replyWithPhoto(img)
+    ctx.reply(img)
+    })
+      .catch(err => {
+        ctx.reply("error")
+    })
+  })
+  .catch(err => {
+    ctx.reply("error")
+  })
+}
 
 bot.launch();
 
